a9a566ae021d49960d84b09d24a7f3329597abc2
Cole McIntosh <colemcintosh6@gmail.com>
2022-05-28 02:15:41 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,13 @@ echo "\
 [general]\n\
 email = \"colemcintosh6@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
+echo "[theme]
+
+primaryColor='#000'
+backgroundColor='#D8D8F6'
+secondaryBackgroundColor='#B18FCF'
+textColor='#000'
+font='sans serif'"
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
