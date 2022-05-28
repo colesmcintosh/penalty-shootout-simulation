4cc8fffda5b5524250e2ebd5384d8959eefc7cc1
Cole McIntosh <colemcintosh6@gmail.com>
2022-05-28 02:24:24 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,9 @@ echo "\
 [general]\n\
 email = \"colemcintosh6@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "[theme]
-primaryColor = ‘#84a3a7’
-backgroundColor = ‘#EFEDE8’
-secondaryBackgroundColor = ‘#fafafa’
-textColor= ‘#424242’
-font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
